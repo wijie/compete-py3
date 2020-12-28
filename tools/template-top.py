aa0297c732bf446cb16ada814fca33533230a207
wijie <agw02010@gmail.com>
2020-12-28 23:26:50 +0900

    Create

---
+++ tools/template-top.py
@@ -0,0 +1,28 @@
+'''
+from math import gcd
+from itertools import accumulate, combinations, permutations, product
+from bisect import bisect, bisect_left
+from collections import Counter, defaultdict, deque
+from heapq import heappush, heappop
+from functools import reduce
+from operator import add, mul, sub, truediv, floordiv, mod
+
+from decimal import *
+getcontext().prec = 500
+
+import string
+ascii = string.ascii_lowercase
+'''
+
+class Name:
+
+
+        return ret
+
+
+# --- これ以降は提出不要 ---
+ans = Name().method(
+    input(),
+    eval(input())
+)
+print(ans)
